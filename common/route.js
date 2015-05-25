a18eeb7dda7faa3ce2fd8bf8230b737890e989cd
bbogeul <bbogeul@gmail.com>
2015-05-25 10:59:12 -0700

    first commit

---
+++ common/route.js
@@ -0,0 +1,68 @@
+
+Router.map(function(){
+    // Specify a different template than 'home'
+this.route('home', {
+  path: '/',
+  layoutTemplate: 'layout',
+     title: 'IV Hair Co | Get your hair done today',
+   action: function() {
+    // this is an instance of RouteController
+    // access to:
+    //  this.params
+    //  this.wait
+    //  this.render
+    //  this.stop
+    //  this.redirect
+
+
+    // render yieldTemplates
+    this.render('header', {to: 'menu'});
+    this.render('footer', {to: 'footer'});
+    this.render('home');
+       
+  }
+});
+    this.route('about', {
+  path: '/about-ivhair',
+  layoutTemplate: 'layout',
+        title: 'About IV Hair Co',
+   action: function() {
+    // this is an instance of RouteController
+    // access to:
+    //  this.params
+    //  this.wait
+    //  this.render
+    //  this.stop
+    //  this.redirect
+
+
+    // render yieldTemplates
+    this.render('header', {to: 'menu'});
+    this.render('footer', {to: 'footer'});
+    this.render('about');
+       
+  }
+});
+        this.route('team', {
+  path: '/meet-the-team',
+  layoutTemplate: 'layout',
+        title: 'Meet the Team ',
+   action: function() {
+    // this is an instance of RouteController
+    // access to:
+    //  this.params
+    //  this.wait
+    //  this.render
+    //  this.stop
+    //  this.redirect
+
+
+    // render yieldTemplates
+    this.render('header', {to: 'menu'});
+    this.render('footer', {to: 'footer'});
+    this.render('team');
+       
+  }
+});
+   
+});
